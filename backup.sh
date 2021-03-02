32430066bd9391eba7f0d6e3c8320917590a83e2
Manitra Andriamitondra <manitra@manitra.net>
2021-03-02 22:55:37 +0300

    put the prefix at a single place and add gz

--- backup.sh
+++ backup.sh
@@ -4,15 +4,15 @@ WP_FOLDER=$1
 WP_DB=$2
 DEST=$3
 
-DATE=$(date +%Y_%m_%d_%H_%M_%S)
+BASE_NAME=backup_$(date +%Y_%m_%d_%H_%M_%S)
 
 #create backup for WP files and WP db
-mkdir backup_$DATE
-tar -czf backup_$DATE/files.tar.gz $WP_FOLDER
-mysqldump $WP_DB -f | gzip > backup_$DATE/db.tar.gz
-tar -cf backup_$DATE.tar backup_$DATE
-rm -rf  backup_$DATE
+mkdir $BASE_NAME
+tar -czf $BASE_NAME/files.tar.gz $WP_FOLDER
+mysqldump $WP_DB -f | gzip > $BASE_NAME/db.tar.gz
+tar -cf $BASE_NAME.tar.gz $BASE_NAME
+rm -rf  $BASE_NAME
 
 #push to destination server and keep 5 most recent files
-scp backup_$DATE.tar $DEST
-rm backup_$DATE.tar
+scp $BASE_NAME.tar.gz $DEST
+rm $BASE_NAME.tar.gz
