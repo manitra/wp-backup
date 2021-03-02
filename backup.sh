b8800ded781b9660a3513c26903fea822086b6b5
Manitra Andriamitondra <manitra@manitra.net>
2021-03-02 22:18:46 +0300

    db export and scp

--- backup.sh
+++ backup.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-FILES=$1
-DEST=$2
+WP_FOLDER=$1
+WP_DB=$2
+DEST=$3
 
 DATE=$(date +%Y_%m_%d_%H_%M_%S)
 
-BACKUP_FILE=files_$DATE.tar.gz
-tar -czf $BACKUP_FILE $1
+#create backup for WP files and WP db
+mkdir backup_$DATE
+tar -czf backup_$DATE/files.tar.gz $WP_FOLDER
+mysqldump $WP_DB -f | gzip > backup_$DATE/db.tar.gz
+tar -cf backup_$DATE.tar backup_$DATE
+rm -rf  backup_$DATE
 
-
-
-
-BACKUP_DB=db_$DATE.tar.gz
+#push to destination server and keep 5 most recent files
+scp backup_$DATE.tar $DEST
+rm backup_$DATE.tar
